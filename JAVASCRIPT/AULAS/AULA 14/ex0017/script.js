f27f3851987c224aaaa2c80792f528fe12b55dc4
José Gonçalves <joseinacio774@gmail.com>
2022-09-01 13:33:49 -0300

    add ex 017

--- JAVASCRIPT/AULAS/AULA 14/ex0017/script.js
+++ JAVASCRIPT/AULAS/AULA 14/ex0017/script.js
@@ -1,1 +1,19 @@
-alert('olá')
+function tabuada() {
+  let number = document.getElementById('txtn')
+  let tabuada = document.getElementById('seltab')
+
+  if (number.value.length == 0) {
+    window.alert('[ERRO 508] Digite Um Numero Por Favor!')
+  } else {
+    let n = Number(number.value)
+    let c = 1
+    tabuada.innerHTML = ''
+    while (c <= 10) {
+      let item = document.createElement('option')
+      item.text = `${n} * ${c} = ${n * c}`
+      item.value = `tab${c}`
+      tabuada.appendChild(item)
+      c++
+    }
+  }
+}
