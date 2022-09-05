dff983b7aea523299a986239320aa11869bce71c
José Gonçalves <joseinacio774@gmail.com>
2022-09-05 17:40:16 -0300

    Finishin Curso JavaScript

--- JAVASCRIPT/AULAS/AULA 16/ex016/modelo/script.js
+++ JAVASCRIPT/AULAS/AULA 16/ex016/modelo/script.js
@@ -1,6 +1,6 @@
 let number = document.getElementById('txtn')
-let tabuada = document.getElementById('seltab')
-let rasut = document.getElementById('ress')
+let lista = document.getElementById('lista')
+let resut = document.getElementById('ress')
 let Valor = []
 //verifica s eo number é um nuber entre 1 e 100
 function isNumero(n) {
@@ -19,10 +19,44 @@ function inLista(n, l) {
   }
 }
 
-function add() {
+function adicionar() {
   if (isNumero(number.value) && !inLista(number.value, Valor)) {
-    alert('ok')
+    Valor.push(Number(number.value))
+    resut.innerHTML = ''
+
+    var item = document.createElement('option')
+    item.text = `Valor ${number.value} adiconado!`
+    lista.appendChild(item)
+    number.value = ''
+    number.focus()
   } else {
     alert('Valor invalido ou ja encontrado na lista!')
+    number.value = ''
+    number.focus()
+  }
+}
+
+function final() {
+  if (Valor == '') {
+    alert('[ERRO404] Impossível Finalizar, nNehum dado adicionado!')
+  } else {
+    let total = Valor.length
+    let maior = Valor[0]
+    let menor = Valor[0]
+    let soma = 0
+    let media = 0
+    //teste d evalor de maior e menor
+    for (let pos in Valor) {
+      soma += Valor[pos]
+      if (Valor[pos] > maior) maior = Valor[pos]
+      if (Valor[pos] < menor) menor = Valor[pos]
+    }
+
+    media = soma / total
+    resut.innerHTML += `<p> O total de elementos adicionados é ${total}!</p>`
+    resut.innerHTML += `<p> O maior valor adicionados é ${maior}!</p>`
+    resut.innerHTML += `<p> O menor valor adicionados é ${menor}!</p>`
+    resut.innerHTML += `<p> A soma d etodos os valores é ${menor}!</p>`
+    resut.innerHTML += `<p> a media de valores adicionados é ${media}!</p>`
   }
 }
